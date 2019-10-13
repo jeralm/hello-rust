1fe36fdb310095fe1e4e51b1d3dc6ac5bf703494
Jean-Raphael Matte <jean.raphael.matte@gmail.com>
2019-10-13 03:48:20 -0400

    make it cool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate ferris_says;
+
+use ferris_says::say;
+use std::io::{ stdout, BufWriter };
+
 fn main() {
-    println!("Hello, world!");
+    let out = b"L'impalpable verve de l'Art, enfin revelee";
+    let width = out.len();
+
+    let mut writer = BufWriter::new(stdout());
+    say(out, width, &mut writer).unwrap();
 }
